f468cec15b9a439638aa5b3394f2f317cc31d8af
okarem <karem.omary@gmail.com>
2020-01-10 23:27:24 +0200

    deployment build and commit

---
+++ build/precache-manifest.d6b5ab3c5f462bcc85b5f0be635f2c19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d12c59acc3b29b88548f4cb3aad7827",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd003d68482becd89cd1",
+    "url": "/static/css/main.29b14fd3.chunk.css"
+  },
+  {
+    "revision": "2be1d4af265f4adb153a",
+    "url": "/static/js/2.8fc7bba8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.8fc7bba8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd003d68482becd89cd1",
+    "url": "/static/js/main.83c8ba4e.chunk.js"
+  },
+  {
+    "revision": "1d8b15ad778fdcea8d83",
+    "url": "/static/js/runtime-main.e5bcd245.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
