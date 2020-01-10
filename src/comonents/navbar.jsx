189cedbdc1a08110a8a53d8aedbaa93421c9f7e8
okarem <karem.omary@gmail.com>
2020-01-10 17:55:44 +0200

    basic router works basic navbar and router changes pages/ components as needed

---
+++ src/comonents/navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <a href="/">home</a>
+      <a href="/favorites">favorites</a>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/favorites.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const FavoritesPage = ()=>{
+    return <React.Fragment>
+        <h1>FavoritesPage</h1>
+    </React.Fragment>
+}
+
+export default FavoritesPage;
